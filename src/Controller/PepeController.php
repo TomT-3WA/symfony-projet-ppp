e669c8f0b58b65d7fde39996fa4cb5a0c03b3124
TomT-3WA <tom.timsit@3wa.io>
2021-05-15 13:58:29 +0100

    first commit

---
+++ src/Controller/PepeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PepeController extends AbstractController
+{
+    /**
+     * @Route("/pepe", name="pepe")
+     */
+    public function index(): Response
+    {
+        return $this->render('pepe/index.html.twig', [
+            'controller_name' => 'PepeController',
+        ]);
+    }
+
+    /**:
+     * @Route("/", name="home")
+     */
+    public function home(): Response {
+        return $this->render('pepe/home.html.twig');
+    }
+}
