140f8c5290c99eb3a3c0e43802071a13678fe15b
TomT-3WA <tom.timsit@3wa.io>
2021-05-18 14:30:30 +0100

    migration type property

--- src/DataFixtures/TrackFixtures.php
+++ src/DataFixtures/TrackFixtures.php
@@ -10,15 +10,15 @@ class TrackFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for ($i=1; $i <= 10; $i++) {
+        for ($i = 1; $i <= 10; $i++) {
             $track = new Track();
             $track->setImage("https://picsum.photos/52")
-                  ->setTitle("Titre de la piste n°$i")
-                  ->setType("Type de la piste n°$i")
-                  ->setDuration("Durée de la piste n°$i")
-                  ->setTempo("BPM de la piste n°$i")
-                  ->setPrice("29,99 €")
-                  ->setCreatedAt(new \DateTime());
+                ->setTitle("Titre de la piste n°$i")
+                ->setType("Type de la piste n°$i")
+                ->setDuration("Durée de la piste n°$i")
+                ->setTempo("BPM de la piste n°$i")
+                ->setPrice("29,99 €")
+                ->setCreatedAt(new \DateTime());
 
             $manager->persist($track);
         }
