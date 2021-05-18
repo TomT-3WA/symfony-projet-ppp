a53c6edcf670c4ccfbf06babb1a1829aed3f2b17
TomT-3WA <tom.timsit@3wa.io>
2021-05-18 17:03:26 +0100

    fixtures Faker

--- src/DataFixtures/TrackFixtures.php
+++ src/DataFixtures/TrackFixtures.php
@@ -2,25 +2,62 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Track;
+use App\Entity\Comment;
 
 class TrackFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for ($i = 1; $i <= 10; $i++) {
-            $track = new Track();
-            $track->setImage("https://picsum.photos/52")
-                ->setTitle("Titre de la piste n°$i")
-                ->setType("Type de la piste n°$i")
-                ->setDuration("Durée de la piste n°$i")
-                ->setTempo("BPM de la piste n°$i")
-                ->setPrice("29,99 €")
-                ->setCreatedAt(new \DateTime());
+        $faker = \Faker\Factory::create('fr_FR');
 
-            $manager->persist($track);
+        // Créer 3 Catégories fakées
+        for ($i = 1; $i <= 3; $i++) {
+            $category = new Category;
+            $category->setTitle($faker->word())
+                ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+
+            // Créer entre 4 et 6 articles
+            for ($j = 1; $j <= mt_rand(4, 6); $j++) {
+
+                $track = new Track();
+
+                $title = '<p>' . join($faker->words(), ' ') . '</p>';
+                $duration = $faker->time('i:s', '5:00');
+                $tempo = $faker->numberBetween(50, 180);
+                $price = $faker->randomFloat(2, 19.99, 99.99);
+
+                $track->setImage($faker->imageUrl(52, 52))
+                    ->setTitle($title)
+                    ->setCategory($category)
+                    ->setType($faker->word())
+                    ->setDuration($duration)
+                    ->setTempo($tempo)
+                    ->setPrice($price)
+                    ->setCreatedAt($faker->dateTimeBetween('-1 year'));
+
+                $manager->persist($track);
+
+                // On donne des commentaires à la track
+                for ($k = 1; $k <= mt_rand(4, 10); $k++) {
+                    $comment = new Comment();
+                    $content = '<p>' . join($faker->paragraphs(2), '</p><p>') . '</p>';
+
+                    $days = (new \DateTime())->diff($track->getCreatedAt())->days;
+
+                    $comment->setAuthor($faker->name)
+                        ->setContent($content)
+                        ->setCreatedAt($faker->dateTimeBetween('-' . $days . 'days'))
+                        ->setTrack($track);
+
+                    $manager->persist($comment);
+                }
+            }
         }
 
         $manager->flush();
